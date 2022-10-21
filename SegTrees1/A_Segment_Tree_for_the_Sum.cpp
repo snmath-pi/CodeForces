74bd7be7ae91265f9a55bfeb930799347a6d2417
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2022-10-21 16:30:29 +0530

    Add files via upload

---
+++ SegTrees1/A_Segment_Tree_for_the_Sum.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std ;
+struct SegTree{
+    vector<long long > sums ;
+    int size ;
+    void init(int n){
+        size = 1 ;
+        while(size <n) size *= 2 ;
+        sums.assign(2*size , 0LL) ;
+
+
+    }
+    
+    void Build(vector<int>&a , int x , int lx , int rx){
+        if(rx - lx == 1){
+            if(lx < (int)a.size()){
+                sums[x] = a[lx] ;
+            }
+            return ;
+        }
+        int m = (lx + rx) / 2 ;
+        Build(a , 2*x +1 , lx , m) ;
+        Build(a , 2*x +2 , m , rx) ;
+        sums[x] = sums[2*x +1 ] + sums[2*x+2] ;
+    }
+    void Build(vector<int> &a){
+        Build(a , 0 , 0 , size) ;
+    }
+    void set(int i , int v , int x , int lx , int rx){
+        if(rx - lx== 1){
+            sums[x] = v; 
+            return ;
+        }
+        int m = (lx + rx) / 2 ;
+        if (i<m) set(i , v , 2*x+1 , lx , m) ;
+        else set(i , v , 2*x+2 , m , rx) ;
+        sums[x] = sums[2*x+1] + sums[2*x +2] ;
+    }
+    void set(int i , int v){
+        set(i , v , 0 , 0 , size) ;
+    }
+    long long sum(int l , int r , int x , int lx , int rx){
+        if(lx >= r || rx <= lx) return 0 ;
+        if(lx >= l && rx <= r) return sums[x] ;
+        int m = (lx + rx)  / 2 ;
+        long long s1= sum(l , r , 2*x+1 , lx , m) ;
+        long long s2 = sum(l , r , 2*x+2 , m , rx) ;
+        return s1+s2 ;
+    }
+    long long sum(int l , int r){
+        return sum(l , r , 0 , 0 ,size) ;
+    }
+};
+int main() {
+    ios_base::sync_with_stdio(false) ;
+    cin.tie(nullptr) ;
+    cout.tie(nullptr) ;
+    int n , m ;
+    cin >> n >> m ;
+    vector<int> a(n) ;
+    for(int i=0; i<n; i++) cin >> a[i] ;
+    SegTree st ;
+    st.init(n) ;
+    st.Build(a) ;
+    while(m--){
+        int op ;
+        cin >> op ;
+        if(op == 1){
+            int i , v ;
+            cin >> i >> v ;
+            st.set(i,v) ;
+        }else{
+            int l , r ;
+            cin >> l >> r ;
+            cout << st.sum(l, r) << "\n" ;
+        }
+    }
+
+    
+    
+}
