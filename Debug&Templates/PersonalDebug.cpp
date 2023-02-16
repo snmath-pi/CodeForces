ff5f8c2e1592eb977cffd8db6d43551b69656198
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-02-16 19:47:54 +0530

    Create PersonalDebug.cpp

---
+++ Debug&Templates/PersonalDebug.cpp
@@ -0,0 +1,37 @@
+/*亲爱的上帝，我求你给我力量来赢得这场战斗。*/
+/* ~~author-> Saksham Negi~~ */
+#include <bits/stdc++.h>
+using namespace std ;
+using i64 = long long;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << "->"; god(x); cerr << "\n";
+#else 
+#define debug(x)
+#endif 
+void god(i64 UWU) { cerr << UWU;}
+void god(int UWU) { cerr << UWU;}
+void god(string UWU) { cerr << UWU;}
+void god(char UWU) { cerr << UWU;}
+void god(double UWU) {cerr << UWU;}
+void god(long double UWU) {cerr << UWU;}
+template<class T> void god(vector<T> V) {cerr << "["; for(T I:V) {god(I); cerr << " ";} cerr << "]";}
+
+void solve() {
+
+}
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    i64 T;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+    return 0;
+}
