e6a5c3851aa355b207872553de1fc1d90d2b0c0f
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-02-16 19:48:33 +0530

    Create CodeforcesN.cpp

---
+++ Debug&Templates/CodeforcesN.cpp
@@ -0,0 +1,26 @@
+/*亲爱的上帝，我求你给我力量来赢得这场战斗。*/
+#include<bits/stdc++.h> 
+using namespace std;
+using i64 = long long;
+#define F0R(i, N) for(int i=0; i<(int)N; i++)
+#define FOR(i, j, N) for(int i=(int)j; i<(int)N; i++)
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+#define vl vector<i64>
+#define vi vector<int>
+template<typename T> bool chkmin(T &a, T b){return (b < a) ? a = b, 1 : 0;}
+template<typename T> bool chkmax(T &a, T b){return (b > a) ? a = b, 1 : 0;}
+
+void solve() {
+    
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int TT=1; 
+    cin >> TT;
+    while(TT--){
+        solve();
+    }
+}
