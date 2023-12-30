b50b129b57750ddceb385e9fc677b13ade938411
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2023-12-30 14:24:33 +0530

    Create D. Number of Segments with Big Sum.cpp

---
+++ ITMO2Pointers/Step2/D. Number of Segments with Big Sum.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define ll long long
+
+const int N = 1e5 + 10;
+const int LG = 8;
+ll A[N];
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(false);
+	
+	int n; ll S;
+	cin >> n >> S;
+	for(int i=0; i<n; i++) {
+		cin >> A[i];
+	}	
+
+	ll res = 0;
+	for(ll x = 0, r = 0, l = 0; r < n; r++) {
+		x += A[r];
+		while(x >= S) {
+			x -= A[l++];
+		}
+		res += l;
+	}
+	cout << res << '\n';
+}
